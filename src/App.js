f27159ef58bcfb6fa6cd741dd16dde585f201a9d
emily-hoang <nhung@nhunghoang.com>
2022-09-28 11:27:19 +1000

    Render user list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import NewUser from './components/Users/NewUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const addUserHandler = (userName, userAge) => {
+    setUserList(previousUserList => {
+      return [
+        ...previousUserList,
+        {
+        name: userName,
+        age: userAge,
+        id: Math.random().toString()}];
+    })
+  }
   return (
     <div>
-      <NewUser />
+      <NewUser onAddUser={addUserHandler}/>
+      <UserList users={userList}/>
     </div>
   );
-}
+} 
 
 export default App;
--- src/components/Users/NewUser.js
+++ src/components/Users/NewUser.js
@@ -3,30 +3,30 @@ import Card from '../UI/Card';
 import styles from './NewUser.module.css';
 import Button from '../UI/Button';
 
-const NewUser = () => {
+const NewUser = props => {
   const [enteredUserName, setEnteredUserName] = useState('');
   const addUserNameHandler = event => {
     setEnteredUserName(event.target.value);
   }
   const [enteredUserAge, setEnteredUserAge] = useState('');
   const addUserAgeHandler = event => {
-    setEnteredUserName(event.target.value);
+    setEnteredUserAge(event.target.value);
   }
   const addUserHandler = event => {
     if(enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       return;
     }
     if (+enteredUserAge < 1) return;
-    // prevent the browser from sending the request -> the page won't reload
+    props.onAddUser(enteredUserName, enteredUserAge);
     event.preventDefault();
     setEnteredUserName('');
     setEnteredUserAge('');
   }
   return <Card className={styles.input}>
     <form onSubmit={addUserHandler}>
-      <label for='userName'>Name</label>
+      <label htmlFor='userName'>Name</label>
       <input type='text' id='userName' name='userName' value={enteredUserName} onChange={addUserNameHandler}></input>
-      <label for='userAge'>Age</label>
+      <label htmlFor='userAge'>Age</label>
       <input type='number' id='userAge' name='userAge' value={enteredUserAge} onChange={addUserAgeHandler}></input>
       <Button type='submit'>Add user</Button>
     </form>
